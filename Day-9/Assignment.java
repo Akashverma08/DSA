859dd82d8b27c0dc6c40a629c9bbec54e406d607
Akash Verma <akashverma0401@gmail.com>
2025-04-20 02:06:54 +0530

    Update Day-9 folder

---
+++ Day-9/Assignment.java
@@ -0,0 +1,23 @@
+public class Assignment {
+    
+    public static boolean twice(int nums[]){
+        boolean result=false;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+                
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int nums[]={1,2,3,4};
+        System.out.println(twice(nums));
+
+
+
+    }
+    
+}
